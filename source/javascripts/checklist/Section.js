e50cbe11b0b170935d1310db4827cbcc7d0a9540
Mike Jansen <mjansen401@gmail.com>
2014-02-08 09:41:23 -0600

    local storage wip

--- source/javascripts/checklist/Section.js
+++ source/javascripts/checklist/Section.js
@@ -1,6 +1,6 @@
 namespace("Checklist", {
   Section: Backbone.Collection.extend({
-    //localStorage: new new Backbone.LocalStorage("Checklist.Section"),
+    // localStorage: new Backbone.LocalStorage("Checklist.Section"),
     model: Checklist.Task,
 
     initialize: function(models, args) {
--- source/javascripts/checklist/SectionView.js
+++ source/javascripts/checklist/SectionView.js
@@ -4,6 +4,10 @@ namespace("Checklist", {
       this.template = args.template;
     },
 
+    events: {
+      'click input': 'updateTask'
+    },
+
     render: function() {
       var data = {name: this.presentSectionName(), tasks: this.presentTasks()};
       this.$el.html(this.template(data));
@@ -18,9 +22,17 @@ namespace("Checklist", {
       return _.map(this.collection.models, function(model) {
         return {
           description: Checklist.DesignerPresentationData[this.collection.name].tasks[model.get('name')],
-          complete: model.get('complete')
+          complete: model.get('complete'),
+          name: model.get('name')
         }
       }, this);
+    },
+
+    updateTask: function(e) {
+      var taskName = $(e.currentTarget).data('name');
+      var complete = $(e.currentTarget).checked;
+      var model = this.collection.findWhere({name: taskName});
+      model.save({complete: complete});
     }
   })
 });
