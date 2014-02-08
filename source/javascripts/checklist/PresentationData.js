908017d6d51225cdc1d18fe102b52764dab02a0a
Mike Jansen <mjansen401@gmail.com>
2014-02-08 11:20:28 -0600

    add blog tasks

--- source/javascripts/checklist/PresentationData.js
+++ source/javascripts/checklist/PresentationData.js
@@ -14,6 +14,13 @@ namespace("Checklist", {
         'git-practice': 'Practice using git add, commit, push, and pull.',
         'merge-conflicts': 'Learn how to resolve merge conflicts.'
       }
+    },
+    'blog': {
+      'wireframes-mockups': 'Create wireframes and mockups for your blog.',
+      'middleman': 'Set up Middleman and create a GitHub repo.',
+      'html5-css3': 'Write HTML5/CSS3 and get comfortable with designing in the browser.',
+      'simple-navigation': 'Create a simple navigation (Blog, Projects, About).',
+      'github-pages': 'Host your blog on GitHub Pages with a custom domain name (free).',
     }
   },
   DeveloperPresentationData: {}
--- source/javascripts/checklist/SeedData.js
+++ source/javascripts/checklist/SeedData.js
@@ -6,7 +6,12 @@ namespace('Checklist', {
     { section: 'git', name: 'install-git' },
     { section: 'git', name: 'git-workflow' },
     { section: 'git', name: 'git-practice' },
-    { section: 'git', name: 'merge-conflicts' }
+    { section: 'git', name: 'merge-conflicts' },
+    { section: 'blog', name: 'wireframes-mockups' },
+    { section: 'blog', name: 'middleman' },
+    { section: 'blog', name: 'html5-css3' },
+    { section: 'blog', name: 'simple-navigation' },
+    { section: 'blog', name: 'github-pages' },
   ],
   DeveloperSeedData: [
   ]
