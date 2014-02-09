7faf0d491b3e578d660ffc98cb6c943b790e961e
Adam Kaplan <akaplan@8thlight.com>
2014-02-08 18:20:28 -0600

    updates layout and organizes stylesheets

--- source/javascripts/checklist/PresentationData.js
+++ source/javascripts/checklist/PresentationData.js
@@ -23,6 +23,28 @@ namespace("Checklist", {
         'simple-navigation': 'Create a simple navigation (Blog, Projects, About).',
         'github-pages': 'Host your blog on GitHub Pages with a custom domain name (free).',
       }
+    },
+    'typography': {
+      tasks: {
+        'master-rules': 'Master the basic rules of typography.',
+        'pair-fonts': 'Learn how to pick and pair fonts (sans-serif, serif, weight, etc.).',
+        'size-fonts': 'Perfect sizing fonts and choosing line heights.',
+        'punctuation': 'Know your punctuation (dumb vs smart quotes, em-dashes).',
+        'web-fonts': 'Use Web fonts (Typekit, Google).'
+      }
+    },
+    'front-end': {
+      tasks: {
+        'working-knowledge': 'Demonstrate a working knowledge of HTML5 and CSS3.',
+        'web-standards': 'Understand web standards and semantic markup.',
+        'cross-browser': 'Utilize cross-browser compatibility techniques (IE 8+).',
+        'guidelines': 'Adhere to guidelines for HTML and CSS “code style” and organization.',
+        'optimization': 'Learn website optimization best practices (image compression, asset management, etc.)',
+        'web-applications': 'Familiarize yourself with front-end inside web applications (Ruby/Rails, Clojure/Joodo).',
+        'pre-processing': 'Learn about the advantages of pre-processing CSS.',
+        'install-sass': 'Install and setup Sass.',
+        'variables-nesting-mixins': 'Demonstrate use of variables, nesting and mixins.'
+      }
     }
   },
   DeveloperPresentationData: {}
